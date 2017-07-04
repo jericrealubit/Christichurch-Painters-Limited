c514da005af5b79179136a450050834ab792644a
jericrealubit <jericrealubit@gmail.com>
2017-07-05 11:27:22 +1200

    laravel/docker

--- cpl/resources/views/welcome.blade.php
+++ cpl/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel by Jeric
                 </div>
 
                 <div class="links">
